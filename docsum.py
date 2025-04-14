f46410cd35e0757e36b1c1e50868ae7c63efe59c
guisabellla <guisabella05@hotmail.com>
2025-04-13 23:42:38 -0700

    main

--- docsum.py
+++ docsum.py
@@ -1,3 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from groq import Groq
+import groq
+
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),  
+)
+
 def llm(text):
     chat_completion = client.chat.completions.create(
         messages=[
@@ -13,21 +24,6 @@ def llm(text):
 
 
 def split_text(text, max_chunk_size=1000):
-    '''
-    Takes a string as input and returns a list of strings
-    that are all smaller than max_chunk_size.
-
-    >>> split_text('abcdefg', max_chunk_size=2)
-    ['ab', 'cd', 'ef', 'g']
-    >>> split_text('this is an example', max_chunk_size=3)
-    ['thi', ' is', ' an', ' ex', 'amp', 'le']
-
-    This is the simplest possible way to split text.
-    Much more sophisticated possibilities.
-    Other more complex algorithms will:
-    1) try not to split words/sentences/paragraphs
-    2) provide overlaps between the chunks
-    '''
     accumulator = []
     while len(text) > 0:
         accumulator.append(text[:max_chunk_size])
@@ -35,20 +31,6 @@ def split_text(text, max_chunk_size=1000):
     return accumulator
 
 def summarize_text(text):
-    '''
-    Our current problem: we cannot summarize large documents.
-    Our solution: recusive summarization.
-    Other solutions exist, no one nows what the best one is.
-    We use recursive sum. because it is easy and illustrates good CS concepts.
-
-    Two step process:
-    1) Split the document into chunks that are the size of the context window.
-       Summarize those chunks using the LLM.
-       This gives us a sequence of smaller documents that we will append together to 
-        create a new document that contains the same information as the original doc
-        but is smaller.
-    2) Call summarize_text on this new smaller document.
-    '''
     prompt = f'''
     Summarize the following text in 1-3 sentences.
 
@@ -63,7 +45,6 @@ def summarize_text(text):
         accumulator = []
         for i, chunk in enumerate(chunks):
             print('i=', i)
-            # recursion is when you call a function inside itself
             summary = summarize_text(chunk)
             accumulator.append(summary)
         summarized_text = ' '.join(accumulator)
@@ -82,9 +63,12 @@ with open(args.filename,'r')as fin:
     text = fin.read()
     print(summarize_text(text))
 '''
+
+'''
 import fulltext
 fulltext.get(args.filename, None)
 print(summarize_text(text))
+'''
 
 if __name__ == '__main__':
     import argparse
@@ -95,23 +79,12 @@ if __name__ == '__main__':
     parser.add_argument('filename')
     args = parser.parse_args()
 
-    from dotenv import load_dotenv
-    load_dotenv()
 
     print('filename=',args.filename)
     with open(args.filename,'r') as fin:
         text = fin.read()
         print(summarize_text(text))
 
-    import os
-    from groq import Groq
-    import groq
-
-    client = Groq(
-        api_key=os.environ.get("GROQ_API_KEY"),  
-    )
-
-   
 
     # one way to solve the problem of too much text for the context window
     # is to remove the "unnecessary" text;
@@ -124,4 +97,4 @@ if __name__ == '__main__':
         #print('text=', text)
         print(summarize_text(text))
 
-print(chat_completion.choices[0].message.content)
+#print(chat_completion.choices[0].message.content)
